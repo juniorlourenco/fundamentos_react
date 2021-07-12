c3cc6140dcc18b03ff177057ca11471976c06b0d
Junior Lourenço <juniorminaj101@gmail.com>
2021-07-12 13:51:28 -0400

    card.css

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Card.css';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
     return (
-        <div>
-            <div>Conteudo</div>
-            <div>{props.titulo}</div>
+        <div className="Card">
+            <div className="Title">Conteudo</div>
+            <div className="Content">{props.titulo}</div>
         </div>
     );
 };
