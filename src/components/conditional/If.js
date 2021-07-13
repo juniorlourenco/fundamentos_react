cc007ad80dfa4c7b729b030b81256fab47eeb2de
Junior Lourenço <juniorminaj101@gmail.com>
2021-07-13 16:16:12 -0400

    conditional render

--- src/components/conditional/If.js
+++ src/components/conditional/If.js
@@ -2,9 +2,24 @@
 
 
 export default props => {
+    const elseChild = props.children.filter(child => {
+        return child.type && child.type.name ==='Else'
+    })[0]
+
+    const ifChildren = props.children.filter(child => {
+        return child !== elseChild
+    })
+    
+    console.log(elseChild);
+
     if(props.test) {
-        return props.children;
-    } else {
+        return ifChildren;
+    } else if(elseChild) {
+        return elseChild
+    } 
+    else {
         return false
     }
 }
+
+export const Else = props=> props.children;
--- src/components/conditional/UsuarioInfo.jsx
+++ src/components/conditional/UsuarioInfo.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
-import If from './If';
+import If, {Else} from './If';
 
 export default props => {
     const usuario = props.usuario || {}
     return (
         <div>
-            <If test={usuario && usuario.nome}>
+            {/* <If test={usuario && usuario.nome}>
             Seja bem vindo <strong>{props.usuario.nome}</strong>
             </If>
             <If test={!usuario || usuario.nome}>
             <p>Seja bem vindo <strong>Anônimo</strong></p>
+            </If> */}
+
+            <If test={usuario && usuario.nome}>
+                Seja bem vindo <strong>{usuario.nome}</strong>
+                <Else>
+                    <p>Seja bem vindo <strong>Amigão</strong></p>
+                </Else>
             </If>
             
         </div>
